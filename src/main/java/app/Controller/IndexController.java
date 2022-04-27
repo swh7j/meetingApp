e06ecf2dc432ccf144d1285263344a3a3405919e
swh7j <swh_77@naver.com>
2022-04-27 11:23:09 +0900

    리액트 연동버젼

--- src/main/java/app/Controller/IndexController.java
+++ src/main/java/app/Controller/IndexController.java
@@ -1,14 +1,17 @@
 package app.Controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class IndexController {
     // 메인페이지 매핑[연결]
-    @GetMapping("/") // url 지정
-    public String main(){
-        return "main";
+    @GetMapping("/main") // url 지정
+    public List<String> main(){
+        return Arrays.asList();
     }
 
     @GetMapping("/error")
