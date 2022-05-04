814e99fa9f2fbb39113c602270ecb435ce0e05c7
kyu05017 <kyu05017@naver.com>
2022-05-04 17:49:28 +0900

    2022-05-04 || 17:49

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,17 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import { ButtonToolbar, Button } from 'react-bootstrap';
+
+import TopMenuComponent from "./component/TopMenuComponent";
 
 function App() {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    fetch('/main')
-        .then(response => response.text())
-        .then(message => {
-          setMessage(message);
-        });
-  },[])
   return (
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-      </header>
-
-    </div>
+      <div className="App">
+          <div>
+              <TopMenuComponent>
+              </TopMenuComponent>
+          </div>
+      </div>
   );
 }
 
--- src/main/java/app/Controller/IndexController.java
+++ src/main/java/app/Controller/IndexController.java
@@ -1,17 +1,18 @@
 package app.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@RestController
+@Controller
 public class IndexController {
     // 메인페이지 매핑[연결]
-    @GetMapping("/main") // url 지정
-    public List<String> main(){
-        return Arrays.asList();
+    @GetMapping({"/", "/error"})
+    public String index() {
+        return "layout";
     }
 
 
