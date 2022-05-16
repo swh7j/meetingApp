fc90d588601449fd43a60b49d0b57f27ee0bc04b
kyu05 <kyu05017@naver.com>
2022-05-16 21:56:25 +0900

    2022-05-16 || 21:56

--- frontend/src/components/Boardwrite.jsx
+++ frontend/src/components/Boardwrite.jsx
@@ -55,7 +55,7 @@ class CreateBoardComponent extends Component {
                 <div className = "container">
                     <div className = "row">
                         <div className = "card col-md-6 offset-md-3 offset-md-3">
-                            <h3 className="text-center">새글을 작성해주세요</h3>
+                            <h3 className="text-center">게시판 글쓰기</h3>
                             <div className = "card-body">
                                 <form>
                                     <div className = "form-group">
