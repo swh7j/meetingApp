9095bfb9f2ad4b73dac27db1c73b4e55d369d056
kyu05017 <kyu05017@naver.com>
2022-05-23 13:49:00 +0900

    2022-05-23 || 13:48

--- frontend/src/components/DetailAcademy.jsx
+++ frontend/src/components/DetailAcademy.jsx
@@ -1,13 +1,17 @@
+/* global kakao */
 import React, { Component } from 'react';
 import AcademyService from '../service/AcademyService';
+import Map from '../components/Map';
 
 class DetailAcademy extends Component {
+
+
     constructor(props) {
         super(props);
 
         this.state = {
             no: this.props.match.params.no,
-            list: {}
+            list: {},
         }
 
     }
@@ -16,6 +20,7 @@ class DetailAcademy extends Component {
         AcademyService.getOneAcademy(this.state.no).then( res => {
             this.setState({list: res.data});
         });
+
     }
 
 
@@ -32,7 +37,6 @@ class DetailAcademy extends Component {
                 <div className = "card col-md-6 offset-md-3">
                     <h3 className ="text-center"> dd </h3>
                     <div className = "card-body">
-
                             <div className = "row">
                                 <label> Title </label> : {this.state.list.aca_NM}
                             </div>
@@ -44,5 +48,4 @@ class DetailAcademy extends Component {
         );
     }
 }
-
 export default DetailAcademy;
